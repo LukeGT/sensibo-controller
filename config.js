013eb3ea393f166979aff3ba31faa8200b6e9590
Luke Tsekouras <luke.tsekouras@gmail.com>
2017-07-10 21:43:04 +1000

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+this.names = {
+  'main': 'frbfV56T',
+  'bedroom': 't7AKK3gg',
+  'study': '563uhxqj',
+};
+this.api_root = 'https://home.sensibo.com/api/v2';
+this.port = 23628;
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const request = require('request-promise-native');
+const body_parser = require('body-parser');
+const _ = require('lodash');
+
+const config = require('./config');
+
+
+const app = express();
+
+app.use(body_parser.json());
+
+const patch_pod = (api_key, id, patch) => {
+
+  const qs = {
+    apiKey: api_key
+  };
+
+  return request.get(config.api_root + '/pods/' + id + '/acStates', {qs, json: true})
+
+  .then( (data) => {
+    const acState = data.result[0].acState;
+    for (let property in patch) {
+      acState[property] = patch[property];
+    }
+    console.log('Applying patch:', id, patch, acState);
+    return request.post(config.api_root + '/pods/' + id + '/acStates', {qs, body: {acState}, json: true});
+  })
+
+  .catch( (error) => {
+    throw {
+      status: 500,
+      reason: error,
+    };
+  });
+};
+
+app.patch('/pods/:name/acState', (req, res) => {
+
+  const pod_id = config.names[req.params.name];
+  let promise = null;
+
+  if (req.params.name === 'all') {
+    const promises = _.values(config.names).map((id) => patch_pod(req.query.apiKey, id, req.body));
+    promise = Promise.all(promises);
+
+  } else if (pod_id) {
+    promise = patch_pod(req.query.apiKey, pod_id, req.body);
+
+  } else {
+    promise = Promise.reject({
+      status: 400,
+      reason: 'Invalid name: ' + req.params.name,
+    });
+  }
+
+  promise.then((data) => {
+    console.log('Success');
+    res.sendStatus(204);
+  })
+  .catch((error) => {
+    console.log('ERROR: Request failed:', error.reason, '\n', req.path, req.query, req.body);
+    res.status(error.status).end();
+  });
+});
+
+app.listen(config.port);
